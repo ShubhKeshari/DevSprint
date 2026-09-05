33dcf2b45d7dd3d8bdc27af031c08801a292b064
ShubhKeshari <shubh1999keshari@gmail.com>
2026-09-05 23:18:55 +0530

    pagination code written

--- pagination/src/App.jsx
+++ pagination/src/App.jsx
@@ -1,22 +1,17 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-  
+  const [count, setCount] = useState(0);
 
   return (
     <>
-    <div>Pagination button add</div>
-    <button>
-      Prev
-    </button>
-
-    <button>
-      Next
-    </button>      
+      <div>Pagination button add</div>
+      <button>Prev</button>
+      <button></button>
+      <button>Next</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
